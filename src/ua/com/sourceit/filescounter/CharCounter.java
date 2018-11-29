ba01e2f8fa6c5882bd2a16e75d0dc3fd1a47c36b
Oleksandr <alexandr.romaniuk@gmail.com>
2018-11-29 18:42:27 +0200

    HW8FileCounters
    Signed-off-by: Oleksandr <alexandr.romaniuk@gmail.com>

--- src/ua/com/sourceit/filescounter/CharCounter.java
+++ src/ua/com/sourceit/filescounter/CharCounter.java
@@ -20,4 +20,52 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CharCounter {
+	TreeMap<Integer, Character> countChar = null;
+	private File file;
+
+	public CharCounter(File file) {
+		this.file = file;
+	}
+
+	public void countChars() {
+		TreeMap<Integer, Character> treeMap = null;
+		HashMap<Character, Integer> countCharLocal = null;
+		try (FileReader fileReader = new FileReader(file);) {
+			char[] chars = new char[(int) file.length()];
+			fileReader.read(chars, 0, (int) file.length());
+
+			List<Character> list = new ArrayList();
+
+			for (char c : chars) {
+				list.add((Character) c);
+			}
+
+			countCharLocal = list.stream().collect(HashMap<Character, Integer>::new, (m, c) -> {
+				if (c != null && !c.equals("") && !c.equals(" ") && c.toString().matches("[A-Za-z]"))
+					if (m.containsKey(c))
+						m.put(c, m.get(c) + 1);
+					else
+						m.put(c, 1);
+			}, HashMap<Character, Integer>::putAll);
+
+			treeMap = new TreeMap<Integer, Character>();
+			for (Entry<Character, Integer> entry : countCharLocal.entrySet())
+				treeMap.put(entry.getValue(), entry.getKey());
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		if (treeMap != null)
+			printReport(treeMap);
+	}
+
+	private void printReport(TreeMap<Integer, Character> toPrint) {
+
+		for (Entry<Integer, Character> item : toPrint.entrySet()) {
+			System.out.println(item.getKey() + " - " + item.getValue());
+		}
+
+	}
 }
+
--- src/ua/com/sourceit/filescounter/Main.java
+++ src/ua/com/sourceit/filescounter/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		FilesCounter f = new FilesCounter();
+		f.countFiles("\\Users\\Oleksandr Romaniuk\\eclipse-workspace\\FilesCounter", "txt");
+		f.countJAVAFiles("\\Users\\Oleksandr Romaniuk\\eclipse-workspace\\FilesCounter", 1);
+		f.countJavaFiles("\\Users\\Oleksandr Romaniuk\\eclipse-workspace\\FilesCounter", 100);
+
+		CharCounter charCounter = new CharCounter(
+				new File("\\Users\\Oleksandr Romaniuk\\eclipse-workspace\\FilesCounter\\test.TXT"));
+		charCounter.countChars();
 
 	}
 
